17d88eb748a75c5e9ecf7b7eccc52fa504da9667
drew-crypto <andrecheicktraore@gmail.com>
2026-08-26 17:34:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
 // @author       You
 // @match        https://www.geo-fs.com/geofs.php?v=3.9
 // @match        https://beta.geo-fs.com/geofs.php?a=22
+// @match        https://beta.geo-fs.com/geofs.php?v=4
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
 // @grant        none
 // ==/UserScript==
